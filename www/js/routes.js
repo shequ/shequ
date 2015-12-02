4ab911a36a02520dc7c67fbaa22792ee412e244f
stevensh <scl1203@163.com>
2015-12-03 07:23:53 +0800

    提交文件

---
+++ www/js/routes.js
@@ -0,0 +1,29 @@
+/**
+ * Created by scl on 2015/12/1.
+ */
+angular.module('syl.routes', [])
+
+  .config(function ($stateProvider,$urlRouterProvider) {
+      $stateProvider
+        .state("home",{
+          url:'/home',
+          templateUrl:'templates/login/home.html'
+        })
+        .state("login",{
+          url:'/login',
+          templateUrl:'templates/login/login.html'
+        })
+        .state("mobile",{
+          url:'/mobile',
+          templateUrl:'templates/login/mobile.html'
+        })
+        .state("captcha",{
+          url:'/captcha',
+          templateUrl:'templates/login/captcha.html'
+        })
+        .state("user-name",{
+          url:'/user-name',
+          templateUrl:'templates/login/user-name.html'
+        })
+    $urlRouterProvider.otherwise("/home");
+  })
---
+++ www/js/services.js
@@ -0,0 +1,4 @@
+/**
+ * Created by scl on 2015/12/1.
+ */
+angular.module('syl.services', [])
